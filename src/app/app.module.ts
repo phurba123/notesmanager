2c93714ec5d596839b1aae654c1982a7cc59d45f
PhursanG <psherpa592@gmail.com>
2022-02-02 06:26:32 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesModule } from './notes/notes.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NotesModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
